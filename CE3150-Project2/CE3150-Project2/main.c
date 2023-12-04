016cafa7c28e7b4c4852769a24fe8359d443ac3f
Adam Camerer <ajc3xc@umsystem.edu>
2023-12-04 16:23:13 -0600

    created initial project workspace

---
+++ CE3150-Project2/CE3150-Project2/main.c
@@ -0,0 +1,18 @@
+/*
+ * CE3150-Project2.c
+ *
+ * Created: 12/4/2023 4:22:53 PM
+ * Author : 13144
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
