042b078b2317301e46d048548add6cdd037efabf
Adam Camerer <ajc3xc@umsystem.edu>
2023-12-08 13:21:43 -0600

    made shippable version of simon game
    did some tweaks, and removed commented out code. Will add time limit next.

--- CE3150-Project2/CE3150-Project2/main.c
+++ CE3150-Project2/CE3150-Project2/main.c
@@ -26,12 +26,6 @@
 
 int SEQUENCE[MAX_LEVEL];
 int LVL = 1;
-int INDEX = 0;
-volatile int DONE = 0;
-int timerOverflows[3]; //array checking whether the timer has overflowed or not
-
-//
-
 
 void generate_simon_pattern(){
 	//since this can be called multiple times, srand shouldn't be here (it regenerates the same pattern)
@@ -41,121 +35,6 @@ void generate_simon_pattern(){
 		SEQUENCE[i] = (rand() % 4) + 1; //generate a random number from 1-4 inclusive
 	}
 }
-
-void delay()
-{
-	_delay_ms(700);
-}
-/*
-void LCD_commander( unsigned char command )
-{
-	LCD_DATA = command;
-	LCD_COMMAND &= ~ (1<<7);	//RS=0
-	LCD_COMMAND &= ~ (1<<6);	//RW=0 write
-	LCD_COMMAND |= (1<<5);	//ENABLE=1
-	_delay_us(1);
-	LCD_COMMAND &= ~ (1<<5);	//ENABLE=0
-	_delay_us(100);
-}
-
-void LCD_char_datashow( unsigned char data )
-{
-	LCD_DATA = data;
-	LCD_COMMAND |= (1<<7);	//RS=1 means data
-	LCD_COMMAND &= ~ (1<<6);	//RW=0 write
-	LCD_COMMAND |= (1<<5);	//ENABLE=1
-	_delay_us(1);
-	LCD_COMMAND &= ~ (1<<5);	//ENABLE=0
-	_delay_us(100);
-}
-
-void LCD_initializer()
-{
-	DDRC = 0xFF;
-	DDRD = 0xF0; // make PORTD output for LCD commands
-	
-	LCD_COMMAND &=~(1<<5);	//ENABLE=0
-	_delay_us(2000);
-	LCD_commander(0x38);		//char size 5*7 16 char in 2 line
-	LCD_commander(0x0E);		//turn lcd on
-	LCD_commander(0x01);		//clean lcd
-	_delay_us(2000);
-	LCD_commander(0x06);		//move cursor to proper position
-}
-
-void LCD_clean(){
-	LCD_commander(0x01);		//clean lcd
-	_delay_us(2000);
-	LCD_commander(0x06);		//move cursor to proper position
-}
-
-void LCD_string_datashow(char* string )
-{
-	unsigned char i = 0;
-	while(string[i]!=0)
-	{
-		LCD_char_datashow(string[i]);
-		i++ ;
-	}
-}
-
-ISR (INT2_vect)
-{
-	int choosen = 0;
-	if(PINB & (1 << 4))
-	{
-		PORTA |= (1<<0);
-		choosen = 1;
-	}
-	else if(PINB & (1 << 5))
-	{
-		PORTA |= (1<<1);
-		choosen = 2;
-	}
-	else if(PINB & (1 << 6))
-	{
-		PORTA |= (1<<2);
-		choosen = 4;
-	}
-	else if(PINB & (1 << 7))
-	{
-		PORTA |= (1<<3);
-		choosen =8;
-	}
-	delay();
-	PORTA = 0x00;
-	if(SEQUENCE[INDEX] != choosen)
-	{
-		LCD_clean();
-		LCD_string_datashow("GAME IS UP");
-		LCD_commander(0xC0);	//next line
-		char level[50];
-		sprintf(level,"YOUR LEVEL: %d", LVL-1);
-		LCD_string_datashow(level);
-		LVL = 0;
-		INDEX = 0;
-		DONE = 1;
-		_delay_ms(1000);
-		return ;
-	}
-	else
-	{
-		INDEX++;
-	}
-	if(INDEX == LVL)
-	{
-		DONE = 1;
-		LCD_clean();
-		LCD_string_datashow("RIGHT");
-		LCD_commander(0xC0);	//next line
-		LCD_string_datashow("NEXT LEVEL");
-		delay();
-		INDEX = 0;
-		return;
-	}
-	return;
-}
-*/
 
 void initialize_ports()
 {
@@ -223,10 +102,11 @@ void display_level_leds()
 	//as there is only 3 leds to display the level
 	if (LVL==0 || LVL > MAX_LEVEL)	return;
 	//display current levels on leds
-	PORTD &= ~(LVL); //(used first 4 leds)
+	PORTD &= ~(LVL); //(used first 4 leds (LED1-4))
 	
 }
 
+/*
 int timer_counter = 1;
 void time_delay() {
 	TCCR1B |= (1 << CS12) | (1 << CS10);
@@ -246,6 +126,7 @@ ISR(TIMER1_OVF_vect) {
 	}
 	TCNT1 = TIMER_VALUE;
 }
+*/
 
 #define HALF_SECOND_TIMER_VAL 0
 #define HALF_SECOND_ITERATIONS 15
@@ -404,11 +285,13 @@ void wait_until_ready_to_play()
 		if (!(PINA & (1 << PINA0)))
 		{
 			waiting = 0;
+			//keep light on and play sound while button is held down
+			PORTD &= ~(1<<PORTD0);
+			play_speaker(1);
 			while (!(PINA & (1 << PINA0))); //wait until unclicked
 		}
 		PORTD ^= (1<<PORTD0);
 	}
-	play_speaker(1);
 	PORTD |= (1<<PORTD0);
 }
 
@@ -508,40 +391,4 @@ int main(void)
 	generate_simon_pattern();
 
 	return 0;
-	
-	/*
-	sei();				// set interrupt enable
-	initialization_function();
-	LCD_initializer();
-	exit;
-	
-	
-	while (LVL <= MAX_LEVEL)
-	{
-		LCD_clean();
-		LCD_string_datashow("LEARN PATTERN");
-		for(char i=0 ; i<LVL ; i++){
-			delay();
-			PORTA = SEQUENCE[i];
-			delay();
-			PORTA = 0x00;
-		}
-		delay();
-		PORTA = 0x00;
-		LCD_clean();
-		LCD_string_datashow("YOUR TIME");
-		LCD_commander(0xC0);
-		char level[50];
-		sprintf(level,"LEVEL: %d", LVL-1);
-		LCD_string_datashow(level);
-		
-		while(DONE == 0);
-		DONE = 0;
-		
-		LVL++;
-	}
-	LCD_clean();
-	LCD_string_datashow("WOW IMPOSSIBLE!");
-	while(1);
-	*/
 }
